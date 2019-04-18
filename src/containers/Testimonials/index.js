32493e49b64251a45908c4d4e4ae498318ebec9a
Cherry Xu <cherryxuqianyun@gmail.com>
2019-04-18 11:38:47 -0700

    testimonial updates

--- src/containers/Testimonials/index.js
+++ src/containers/Testimonials/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import img from '../../assets/baby2.jpg';
 import imgActive from '../../assets/sarah.jpeg';
-import avatar1 from '../../assets/sam.jpg';
+import ella from '../../assets/review-ella.png';
+import ellaBW from '../../assets/review-ella-bw.jpg';
+import vanessa from '../../assets/review-vanessa.png';
+import vanessaBW from '../../assets/review-vanessa-bw.jpg';
+import madeline from '../../assets/review-madeline.jpeg';
+import madelineBW from '../../assets/review-madeline-bw.jpg';
 import { Icon } from 'semantic-ui-react';
 import selectTestimonial from '../../actions/selectTestimonial';
 import CustomerAvatar from './CustomerAvatar';
 import './testimonials.css';
 
 const reviews = [
-  'I’m so happy I found mama mobile. This is an amazing service for any parent who feels that they cannot take time out of their busy schedules to take care of themselves, and who would appreciate maintaining their health from within the comfort of their home.',
-  'I sent Hubby & baby upstairs, dog flaked out on the couch, and closed the curtains. Once my body was on the massage table, I was transported. She had the spa music, comfortable blankets, and gave me an incredible massage. I soon forgot the pile of clean laundry I had forgotten on the chair, and that I was surrounded by my son’s toys, and my hip which has been bugging me since I gave birth feels so much better already.',
-  'Great to be at home after kids are in bed. I am definitely going to re-book in the future. Highly recommended!'
+  'This service made it so easy to schedule a session as we were told that extra time is allotted for feedings or fussiness so I felt comfortable knowing there would be some flexibility there plus we’re at home with all the conveniences baby may need and where we’re all most comfortable.',
+  'Being on bedrest and 37 weeks pregnant I was dying for some sort of relaxation, but not being able to leave the house made me feeling that would be impossible. It was amazing to have this service brought to my own home.',
+  'She arrived post kiddie-bedtime, was friendly and professional and even stepped over our two large dogs who insisted on lying under the massage table. We both felt great during and after our massages. Highly recommended! 🙌🏻'
 ];
 
 const Testimonials = ({ testimonial, selectTestimonial }) => (
@@ -29,13 +34,13 @@ const Testimonials = ({ testimonial, selectTestimonial }) => (
       <p>{reviews[testimonial]}</p>
       <div id='customer-avatar-wrapper'>
         <div onClick={() => selectTestimonial(0)}>
-          <CustomerAvatar active={testimonial === 0 ? true : false} img={avatar1} imgActive={imgActive} name='Cherry X' type='Postnatal' />
+          <CustomerAvatar active={testimonial === 0 ? true : false} img={ellaBW} imgActive={ella} name='Ella' type='Postnatal' />
         </div>
         <div onClick={() => selectTestimonial(1)}>
-          <CustomerAvatar active={testimonial === 1 ? true : false} img={avatar1} imgActive={imgActive} name='Cherry X' type='Postnatal' />
+          <CustomerAvatar active={testimonial === 1 ? true : false} img={vanessaBW} imgActive={vanessa} name='Vanessa' type='Prenatal' />
         </div>
         <div onClick={() => selectTestimonial(2)}>
-          <CustomerAvatar active={testimonial === 2 ? true : false} img={avatar1} imgActive={imgActive} name='Cherry X' type='Postnatal' />
+          <CustomerAvatar active={testimonial === 2 ? true : false} img={madelineBW} imgActive={madeline} name='Madeline' type='Postnatal' />
         </div>
       </div>
     </div>
