398136c109c25a8d7950cbd5bf2d66713323082a
Cherry Xu <cherryxuqianyun@gmail.com>
2019-07-04 09:40:42 -0700

    redirect again...

---
+++ src/containers/Prices/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import ScrollToTop from '../../components/ScrollTop';
+
+const Career = () => (
+  <div id='career'>
+    <Helmet>
+        <title>Mama Mobile Massage | Prices</title>
+        <meta name="description" content="We offer the best, transparent prices to our mamas, always!" />
+    </Helmet>
+    <ScrollToTop />
+  </div>
+)
+
+export default Career;
